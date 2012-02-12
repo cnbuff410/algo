7d0a7895f867fdc53df304a5f83ca2892fa2dcd6
windstorm <likunarmstrong@gmail.com>
2012-02-11 23:56:53 -0700

    Fix the format of skiplist source files

--- skiplist/skiplist.go
+++ skiplist/skiplist.go
@@ -1,98 +1,108 @@
 package main
 
 import (
-    "fmt"
-    "math/rand"
-    "time"
+	"fmt"
+	"math/rand"
+	"time"
 )
 
 const (
-    MaxInt = int(^uint(0) >> 1)
-    MinInt = -MaxInt - 1
+	MaxInt = int(^uint(0) >> 1)
+	MinInt = -MaxInt - 1
 )
 
 type Node struct {
-    Value int
-    Next []*Node
+	Value int
+	Next  []*Node
 }
 
 type Skiplist struct {
-    Head *Node
-    Level int
-    Length int
+	Head   *Node
+	Level  int
+	Length int
 }
 
 func New(maxLevel int) *Skiplist {
-    // Allocation head points
-    head := &Node{MinInt, make([]*Node, maxLevel)}
-    return &Skiplist{Level:0, Head:head, Length:0} // Return header
+	// Allocation head points
+	head := &Node{MinInt, make([]*Node, maxLevel)}
+	return &Skiplist{Level: 0, Head: head, Length: 0} // Return header
 }
 
 func (sl *Skiplist) Insert(value int) {
-    // Calculate level
-    level := 0
-    t := time.Now()
-    rand.Seed((int64)(t.Nanosecond()))
-    for rand.Float64() < 0.5 {
-        level++
-    }
-    if level + 1 > sl.Level {sl.Level = level + 1}
+	// Calculate level
+	level := 0
+	t := time.Now()
+	rand.Seed((int64)(t.Nanosecond()))
+	for rand.Float64() < 0.5 {
+		level++
+	}
+	if level+1 > sl.Level {
+		sl.Level = level + 1
+	}
 
-    // Init new node
-    newNode := &Node{Value: value, Next: make([]*Node, level+1)}
+	// Init new node
+	newNode := &Node{Value: value, Next: make([]*Node, level+1)}
 
-    // Find the place to insert
-    for i := sl.Level-1; i >= 0; i-- {
-        // Go down if value is bigger or ptr is the last one element
-        var ptr *Node
-        for ptr = sl.Head; ptr.Next[i] != nil; ptr = ptr.Next[i] {
-            if ptr.Next[i].Value > value {break}
-        }
-        // Insert
-        if i <= level {
-            newNode.Next[i] = ptr.Next[i]
-            ptr.Next[i] = newNode
-        }
-    }
-    sl.Length++
+	// Find the place to insert
+	for i := sl.Level - 1; i >= 0; i-- {
+		// Go down if value is bigger or ptr is the last one element
+		var ptr *Node
+		for ptr = sl.Head; ptr.Next[i] != nil; ptr = ptr.Next[i] {
+			if ptr.Next[i].Value > value {
+				break
+			}
+		}
+		// Insert
+		if i <= level {
+			newNode.Next[i] = ptr.Next[i]
+			ptr.Next[i] = newNode
+		}
+	}
+	sl.Length++
 }
 
 func (sl *Skiplist) Delete(value int) {
-    if !sl.Contains(value) {
-        var prePtr *Node
-        for i := sl.Level-1; i >= 0; i-- {
-            for ptr := sl.Head; ptr.Next[i] != nil; ptr = ptr.Next[i] {
-                if ptr.Value == value {
-                    prePtr.Next[i] = ptr.Next[i].Next[i]
-                    break
-                }
-                if ptr.Next[i].Value > value {break}
-            }
-        }
-    }
+	if !sl.Contains(value) {
+		var prePtr *Node
+		for i := sl.Level - 1; i >= 0; i-- {
+			for ptr := sl.Head; ptr.Next[i] != nil; ptr = ptr.Next[i] {
+				if ptr.Value == value {
+					prePtr.Next[i] = ptr.Next[i].Next[i]
+					break
+				}
+				if ptr.Next[i].Value > value {
+					break
+				}
+			}
+		}
+	}
 }
 
 func (sl *Skiplist) Print() {
-    for i := sl.Level-1; i >= 0; i-- {
-        fmt.Printf("Level %d: ", i)
-        for ptr := sl.Head; ptr.Next[i] != nil; ptr = ptr.Next[i] {
-            fmt.Printf("%v ", ptr.Next[i].Value)
-        }
-        fmt.Printf("\n\n")
-    }
+	for i := sl.Level - 1; i >= 0; i-- {
+		fmt.Printf("Level %d: ", i)
+		for ptr := sl.Head; ptr.Next[i] != nil; ptr = ptr.Next[i] {
+			fmt.Printf("%v ", ptr.Next[i].Value)
+		}
+		fmt.Printf("\n\n")
+	}
 }
 
 func (sl *Skiplist) Contains(value int) bool {
-    var i int
-    for i = sl.Level-1; i >= 0; i-- {
-        for ptr := sl.Head; ptr.Next[i] != nil; ptr = ptr.Next[i] {
-            if ptr.Next[i].Value == value {return true}
-            if ptr.Next[i].Value > value {break}
-        }
-    }
-    return false
+	var i int
+	for i = sl.Level - 1; i >= 0; i-- {
+		for ptr := sl.Head; ptr.Next[i] != nil; ptr = ptr.Next[i] {
+			if ptr.Next[i].Value == value {
+				return true
+			}
+			if ptr.Next[i].Value > value {
+				break
+			}
+		}
+	}
+	return false
 }
 
 func (sl *Skiplist) Len() int {
-    return sl.Length
+	return sl.Length
 }
--- skiplist/skiplist_test.go
+++ skiplist/skiplist_test.go
@@ -1,60 +1,64 @@
 package main
 
 import (
-    "testing"
+	"testing"
 )
 
 func Test_skiplist(t *testing.T) {
-    sl := New(32)
-    sl.Insert(3)
-    sl.Insert(16)
-    sl.Insert(17)
-    sl.Insert(5)
-    sl.Insert(25)
-    sl.Insert(1)
-    sl.Insert(13)
-    sl.Insert(16)
-    sl.Insert(12)
-    sl.Insert(5)
-    sl.Print()
+	sl := New(32)
+	sl.Insert(3)
+	sl.Insert(16)
+	sl.Insert(17)
+	sl.Insert(5)
+	sl.Insert(25)
+	sl.Insert(1)
+	sl.Insert(13)
+	sl.Insert(16)
+	sl.Insert(12)
+	sl.Insert(5)
+	sl.Print()
 }
 
 func populate(n int) *Skiplist {
-    l := New(n)
-    for i := 0; i < n; i++ {
-        l.Insert(i)
-    }
-    return l
+	l := New(n)
+	for i := 0; i < n; i++ {
+		l.Insert(i)
+	}
+	return l
 }
 
 var l100 = populate(100)
+
 func BenchmarkLookup100(b *testing.B) {
-        l := l100
-        for i := 0; i < b.N; i++ {
-                l.Contains(i)
-        }
+	l := l100
+	for i := 0; i < b.N; i++ {
+		l.Contains(i)
+	}
 }
 
 var l1000 = populate(1000)
+
 func BenchmarkLookup1000(b *testing.B) {
-        l := l1000
-        for i := 0; i < b.N; i++ {
-                l.Contains(i)
-        }
+	l := l1000
+	for i := 0; i < b.N; i++ {
+		l.Contains(i)
+	}
 }
 
 var l10000 = populate(10000)
+
 func BenchmarkLookup10000(b *testing.B) {
-        l := l10000
-        for i := 0; i < b.N; i++ {
-                l.Contains(i)
-        }
+	l := l10000
+	for i := 0; i < b.N; i++ {
+		l.Contains(i)
+	}
 }
 
 var l100000 = populate(100000)
+
 func BenchmarkLookup100000(b *testing.B) {
-        l := l100000
-        for i := 0; i < b.N; i++ {
-                l.Contains(i)
-        }
+	l := l100000
+	for i := 0; i < b.N; i++ {
+		l.Contains(i)
+	}
 }
